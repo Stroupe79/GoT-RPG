13cb0439fb91dec300fce141b50a7eb9c2b8c67c
Chris Stroupe <cstroupe79@gmail.com>
2019-03-14 14:27:46 -0500

    working on adding images - functions all fire at once for some reason

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,49 +1,60 @@
 // variable declarations
 $(document).ready(function() {
 
-var usrHealth
-var userAttack
-var cntrAttack
-var defHealth
+var usrHealth = 100;
+var userAttack = 6;
+var cntrAttack = 5;
+var defHealth = 100;
 var snow
 var jamie
 var dany
 var stannis
 
 
-    console.log("Ready")
+    console.log("Ready");
 
-$("#picChange").attr('src', './assets/images/stannis.jpg');
+// click events
 
-$("#snow").click(function() {
-    console.log("Test");
-    picChange("#snow");
-});
+$("#snow").click(attPic("snow.jpg")); 
+$("#jamie").click(attPic("jamie.jpg")); 
+$("#dany").click(attPic("dany.jpg")); 
+$("#stannis").click(attPic("stannis.jpg")); 
+$("#battle").click( battle());
 
-function picChange(){
-$("#picChange").show(function() {
-   if ((this).attr === "./assets/images/snow.jpg"){
-        $(this).attr('src', './assets/images/stannis.jpg');
-        console.log("Ran");
-   }
-});
-}
+
+
+
+
 // functions - foreach click add to userAttack - remove from defHealth - remove from usrHealth
-$("#battle").click(function() {
-    console.log("Test");
-    picChange();
+
+
+// $("select").change(function () {
+//     var str = "";
+//     $( "select option:selected" ).each(function() {
+//       str += $( this ).text() + " ";
+//     });
+//     $( "div" ).text( str );
+//   })
+//   .change();
+
+  
+  function attPic(attacker){
+      $("#attackSpace").append("<img src=./assets/images/" + attacker  + ' height="300px"' + "class='col-md-3'" + ">"  );
+      console.log(attacker);
+    }
+
+
+  function battle(){
+      defHealth = defHealth - userAttack;
+      userAttack = userAttack + 6;
+      usrHealth = usrHealth - cntrAttack;
+      console.log(defHealth);
+      console.log(userAttack);
+      console.log(usrHealth);
+  };
+
 });
 
-$("select").change(function () {
-    var str = "";
-    $( "select option:selected" ).each(function() {
-      str += $( this ).text() + " ";
-    });
-    $( "div" ).text( str );
-  })
-  .change();
-
-});
 
 // $(selector).append(content);
 
